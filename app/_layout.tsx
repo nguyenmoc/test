835c8b2a4c71c30b01b484faaca95e7f49c16a0e
tuhv <tudahe163675@fpt.edu.vn>
2025-10-01 16:23:32 +0700

    disable warning expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,13 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+// ✅ Tắt tất cả warnings
+LogBox.ignoreAllLogs();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
